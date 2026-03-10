b7214a6df5e4d43650daed97ed29d02aeded10bd
Carletoz <carletozGit@gmail.com>
2026-03-10 13:19:42 -0500

    conditionals

---
+++ conditionals/08_exercises.py
@@ -0,0 +1,83 @@
+# x = 2
+
+# if x >= 0:
+#     print("el numero es positivo")
+# else:
+#     print("el numero es negativo")
+
+#2
+# # age = int(input("enter your age: "))
+# if age >= 18:
+#     print("your legal")
+# else:
+#     print("your are not legal")
+
+#3
+# input_string = str(input("ingrese un texto: "))
+
+# if not input_string:
+#     print("la cadena de texto esta vacia")
+# else:
+#     print("la cadena de texto tiene contenido")
+
+# print(input_string)
+
+# #4
+# y = int(input("ingrese un numero: "))
+# z = int(input("ingrese otro numero: "))
+
+# if y>z:
+#     print(f"el numero {y} es mayor al numero {z}")
+# elif z>y:
+#     print(f"el numero {z} es mayor al numero {y}")
+# else:
+#     print("Los numeros son iguales.")
+
+#5
+# y = int(input("ingrese un numero: "))
+
+# if y%3 == 0 and y%5 ==0:
+#     print("el numero es divisible por 3 y 5")
+# else:
+#     print("el numero no es divisible por 3 y 5")
+
+#6
+# z = int(input("ingrese un numero: "))
+
+# if z== 0 or z<0:
+#     print("el numero no puede ser cero ni negativo")
+# elif z%2 == 0 :
+#     print("el numero es par")
+# else:
+#     print("el numero es impar")
+
+#7
+# edad = int(input("ingrese su edad: "))
+
+# if edad >= 18:
+#     print("el ciudadano puede votar")
+# elif edad == 16 or edad == 17:
+#     print("el ciudadano puede votar con permiso especial")
+# else:
+#     print("el ciudadano no puede votar")
+
+#8
+# password_storaged = "pepito"
+
+# password_input=str(input("ingrese su contraseña: "))
+
+# if password_input == password_storaged:
+#     print("bienvenido a su portal")
+# else:
+    # print("Credenciales incorrectas")
+
+
+#9 
+# numero = int(input("ingrese un numero: "))
+
+# if numero >= 10 and numero <=20:
+#     print("el numero esta entre 10 y 20")
+# else:
+#     print("el numero no esta entre 10 y 20")
+
+#10
