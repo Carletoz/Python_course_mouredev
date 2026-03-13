6855be0da04e3fe81a732a42ac1badb36285a7e9
Carletoz <carletozGit@gmail.com>
2026-03-13 13:32:05 -0500

    bucles completed

---
+++ bucles/09_exercises.py
@@ -0,0 +1,84 @@
+# x=0 
+
+# while x<=10:
+#     print(f"el numero es: {x}")
+#     x += 1
+# else:
+#     print("mi condicion es mayor o igual a 10")
+
+    
+# #2
+
+# list1 = [10, 20, 30, 40, 50]
+
+# for element in list1:
+#     print(element)
+
+# #3
+# y=1
+# sum = 0
+
+# while y<=100:
+#     sum += y
+#     y += 1
+#     print(f"el numero es: {y}")
+
+
+#4
+# word = "python"
+# i =0 
+
+# for letter in word:
+#     print(letter)
+
+
+#5
+# i=1
+
+# while i <= 50:
+#     if i % 7 == 0:
+#         print(f"se detiene la ejecucion {i}")
+#         break
+#     i+=1
+
+#6
+
+# dict1= {"name": "Brais", "age": 37, "country": "Galicia"}
+
+# for value in dict1.values():
+#     print(value)
+
+#7
+# i=1
+
+# while i <=20:
+#     if i % 2 == 0:
+#         print(i)
+#     i+=1
+    
+#8
+
+# i = 1
+
+# for i in range(10,0,-1):
+#     print(i)
+
+#9
+
+list1= [30, 10, 30, 20, 30, 40]
+
+contador = 0
+
+for numero in list1:
+    if numero == 30:
+        contador += 1 
+print(contador)
+
+#10
+names = ["Sara", "Brais", "Pedro"]
+
+for nombres in names:
+    if nombres == "Brais":
+        print("se encontro a brais")
+        break
+    
